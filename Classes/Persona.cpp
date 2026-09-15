f3d8862b704e3499e781b123a303822ce018edd3
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2026-09-14 22:27:58 -0300

    se crean las clases

---
+++ Classes/Persona.cpp
@@ -0,0 +1,28 @@
+#include "Persona.hpp"
+#include <iostream>
+
+Persona::Persona() : nombre(""), edad(0) {}
+
+Persona::Persona(const std::string& nombre, int edad)
+    : nombre(nombre), edad(edad) {}
+
+Persona::~Persona() {}
+
+std::string Persona::getNombre() const {
+    return nombre;
+}
+
+int Persona::getEdad() const {
+    return edad;
+}
+
+void Persona::setNombre(const std::string& nombre) {
+    this->nombre = nombre;
+}
+
+void Persona::setEdad(int edad) {
+    this->edad = edad;
+}
+
+void Persona::mostrarInformacion() const {
+    std::cout << "Nombre: " << nombre << " | Edad: " << edad;
