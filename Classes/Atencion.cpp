82ea18fda1d8eb2b873b80b6e8a0618a74b9337c
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2026-09-15 15:43:21 -0300

    se crean las clases

---
+++ Classes/Atencion.cpp
@@ -0,0 +1,36 @@
+#include "Atencion.hpp"
+#include <iostream>
+
+Atencion::Atencion() { 
+  idPaciente = ""; 
+  nombrePaciente = ""; 
+  edadPaciente = 0; 
+  servicio = ""; 
+}
+
+
+Atencion::Atencion(const Paciente& paciente) { 
+  idPaciente = paciente.getId(); 
+  nombrePaciente = paciente.getNombre(); 
+  edadPaciente = paciente.getEdad(); 
+  servicio = paciente.getServicioDestino(); 
+}
+
+
+Atencion::Atencion(const std::string& id, const std::string& nombre, int edad, const std::string& servicio) { 
+  idPaciente = id; 
+  nombrePaciente = nombre; 
+  edadPaciente = edad; 
+  this->servicio = servicio; 
+}
+
+Atencion::~Atencion() { }
+
+std::string Atencion::getIdPaciente() const { return idPaciente; }
+std::string Atencion::getNombrePaciente() const { return nombrePaciente; }
+int Atencion::getEdadPaciente() const { return edadPaciente; }
+std::string Atencion::getServicio() const { return servicio; }
+
+void Atencion::mostrarDetalle() const {
+    std::cout << "Nombre: " << nombrePaciente << " | Edad: " << edadPaciente << " | Departamento: " << servicio << "\n";
+}
