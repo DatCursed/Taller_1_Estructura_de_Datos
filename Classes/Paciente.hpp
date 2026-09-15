abfb250efc1ec735dbd642380118bd0cec247d01
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2026-09-14 23:21:03 -0300

    se crean las clases

---
+++ Classes/Paciente.hpp
@@ -0,0 +1,26 @@
+#ifndef PACIENTE_HPP
+#define PACIENTE_HPP
+
+#include "Persona.hpp"
+#include <string>
+
+class Paciente : public Persona {
+private:
+    std::string id;
+    std::string servicioDestino;
+
+public:
+    Paciente();
+    Paciente(const std::string& id, const std::string& nombre, int edad, const std::string& servicioDestino);
+    ~Paciente() override;
+
+    std::string getId() const;
+    std::string getServicioDestino() const;
+
+    void setId(const std::string& id);
+    void setServicioDestino(const std::string& servicioDestino);
+
+    void mostrarInformacion() const override;
+};
+
+#endif // PACIENTE_HPP
