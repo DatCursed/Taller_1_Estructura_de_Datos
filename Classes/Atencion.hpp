406f1a75537f0f540d1be191228d9f3d5e802a64
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2026-09-15 15:32:54 -0300

    se crean las clases

---
+++ Classes/Atencion.hpp
@@ -0,0 +1,28 @@
+#ifndef ATENCION_HPP
+#define ATENCION_HPP
+
+#include "Paciente.hpp"
+#include <string>
+
+class Atencion {
+private:
+    std::string idPaciente;
+    std::string nombrePaciente;
+    int edadPaciente;
+    std::string servicio;
+
+public:
+    Atencion();
+    Atencion(const Paciente& paciente);
+    Atencion(const std::string& id, const std::string& nombre, int edad, const std::string& servicio);
+    ~Atencion();
+
+    std::string getIdPaciente() const;
+    std::string getNombrePaciente() const;
+    int getEdadPaciente() const;
+    std::string getServicio() const;
+
+    void mostrarDetalle() const;
+};
+
+#endif // ATENCION_HPP
