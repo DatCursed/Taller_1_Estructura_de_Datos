c3d64b354e9165ab7a5e71f5e2a181a4945abd6d
DiegoSB20 <diego.seco@alumnos.ucn.cl>
2026-09-15 21:30:28 -0300

    se crean las clases

--- Classes/Atencion.cpp
+++ Classes/Atencion.cpp
@@ -1,37 +1,52 @@
 #include "Atencion.hpp"
 #include <iostream>
-//using namespace std;
 
-Atencion::Atencion() { 
-  idPaciente = ""; 
-  nombrePaciente = ""; 
-  edadPaciente = 0; 
-  servicio = ""; 
+using namespace std;
+
+Atencion::Atencion() {
+    idPaciente = "";
+    nombrePaciente = "";
+    edadPaciente = 0;
+    servicio = "";
 }
 
-
-Atencion::Atencion(const Paciente& paciente) { 
-  idPaciente = paciente.getId(); 
-  nombrePaciente = paciente.getNombre(); 
-  edadPaciente = paciente.getEdad(); 
-  servicio = paciente.getServicioDestino(); 
+Atencion::Atencion(const Paciente& paciente) {
+    idPaciente = paciente.getId();
+    nombrePaciente = paciente.getNombre();
+    edadPaciente = paciente.getEdad();
+    servicio = paciente.getServicioDestino();
 }
 
-
-Atencion::Atencion(const std::string& id, const std::string& nombre, int edad, const std::string& servicio) { 
-  idPaciente = id; 
-  nombrePaciente = nombre; 
-  edadPaciente = edad; 
-  this->servicio = servicio; 
+Atencion::Atencion(const string& id, const string& nombre, int edad, const string& servicio) {
+    idPaciente = id;
+    nombrePaciente = nombre;
+    edadPaciente = edad;
+    this->servicio = servicio;
 }
 
-Atencion::~Atencion() { }
+Atencion::~Atencion() {
+}
 
-std::string Atencion::getIdPaciente() const { return idPaciente; }
-std::string Atencion::getNombrePaciente() const { return nombrePaciente; }
-int Atencion::getEdadPaciente() const { return edadPaciente; }
-std::string Atencion::getServicio() const { return servicio; }
+string Atencion::getIdPaciente() const {
+    return idPaciente;
+}
+
+string Atencion::getNombrePaciente() const {
+    return nombrePaciente;
+}
+
+int Atencion::getEdadPaciente() const {
+    return edadPaciente;
+}
+
+string Atencion::getServicio() const {
+    return servicio;
+}
 
 void Atencion::mostrarDetalle() const {
-    std::cout << "Nombre: " << nombrePaciente << " | Edad: " << edadPaciente << " | Departamento: " << servicio << "\n";
+    cout << "Nombre: " << nombrePaciente
+         << " | Edad: " << edadPaciente
+         << " | Departamento: " << servicio << "\n";
 }
+
+
